df1f6496e419ac3ba7f01cd821e517c88a212766
webnetkz <dbqqbq@gmail.com>
2021-06-14 18:10:26 +0600

    update

--- app/views/main/index.php
+++ app/views/main/index.php
@@ -1,17 +1,3 @@
-<!-- <div class="slider">
-    <img src="/public/img/banners/arrowLeft.svg" id="backSlide" class="arrow">
-    <div class="imgSlider">
-        <img src="/public/img/banners/1.jpg" class="slides">
-        <div class="paginationSlider">
-            <span class="datSlider datSliderActive" slide="1" onclick="clickDatSlide(this);">•</span>
-            <span class="datSlider" slide="2" onclick="clickDatSlide(this);">•</span>
-            <span class="datSlider" slide="3" onclick="clickDatSlide(this);">•</span>
-            <span class="datSlider" slide="4" onclick="clickDatSlide(this);">•</span>
-            <span class="datSlider" slide="5" onclick="clickDatSlide(this);">•</span>
-        </div>
-    </div>
-    <img src="/public/img/banners/arrowRight.svg" id="nextSlide" class="arrow arrowTwo">
-</div> -->
 <?php require_once 'slider.php';?>
 <!-- <script src="/public/js/slider.js"></script> -->
 <style>
@@ -27,18 +13,6 @@
 <div class="centerContent">
     <h3><?=$news?></h3>
     <?php require_once 'newsSlider.php'; ?>
-    <!-- <div class="newsContent">
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-        <div class="showNews"></div>
-    </div> -->
     <h3><?=$newsLib?></p></h3>
 
 
